97a750c862cfeb9c8ad0c29f990876bce8d2d20a
TwigTheCat <TwigTheCat@disroot.org>
2024-09-14 18:26:10 -0500

    Added nixwebr.ing stuff

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "ags config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells."x86_64-linux".default =
+      pkgs.mkShell { 
+        packages = [
+          pkgs.vscode-langservers-extracted
+        ];
+      };
+  };
+}
